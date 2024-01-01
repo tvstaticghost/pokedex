d9cde3c87885b5b7139014d77b37cb7249e94b70
tvstaticghost <ray.m.arnold@gmail.com>
2024-01-01 13:33:20 -0500

    Added styling to the pokedex and added power button functionality

--- main.js
+++ main.js
@@ -1,4 +1,59 @@
-function runApi() {
-    const searchEntry = '';
-    const apiURL = `https://pokeapi.co/api/v2/pokemon/${searchEntry}`;
+const pokemon = {
+    picture: '',
+    name: '',
+    id: '',
+    type: ''
 }
+
+function runApi() {
+    const searchEntry = 'eevee';
+    const apiURL = `https://pokeapi.co/api/v2/pokemon/${searchEntry}`;
+
+    fetch(apiURL)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+        })
+}
+
+const powerBtn = document.getElementById('power-btn');
+powerBtn.addEventListener('click', togglePower);
+let isOn = false;
+
+function togglePower() {
+
+    if (!isOn) {
+        isOn = true;
+    }
+    else {
+        isOn = false;
+    }
+    console.log(isOn);
+
+    toggleScreen();
+}
+
+function toggleScreen() {
+    const screenArray = [
+        document.getElementById('pokedex-screen'),
+        document.getElementById('screen-sub'),
+        document.getElementById('screen-num'),
+        document.getElementById('screen-type'),
+        document.getElementById('pokemon-name')
+    ];
+
+    if (isOn) {
+        for (let i = 0; i < screenArray.length; i++) {
+            screenArray[i].classList.remove('screen__off');
+            screenArray[i].classList.add('animation__add');
+        }
+    }
+    else {
+        for (let i = 0; i < screenArray.length; i++) {
+            screenArray[i].classList.add('screen__off');
+            screenArray[i].classList.remove('animation__add');
+        }
+    }
+}
+
+runApi();
